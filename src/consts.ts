ab62234b340ee90855433facb5b00cfc79b5da50
Blackwood416 <animeweaver959@gmail.com>
2026-06-22 18:33:05 +0800

    feat: add migrated blog routes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Blackwood's Blogs";
+export const SITE_DESCRIPTION = 'Blackwood 的技术文章档案。';
+export const SITE_AUTHOR = 'Blackwood';
